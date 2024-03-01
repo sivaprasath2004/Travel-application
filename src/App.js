0f96ec0754321dd3dddbb82f0407ccff6b5a6b42
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-03-01 22:01:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Routes,Route, Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Book from './Book'
+import Login from './Login'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <nav>
+      <div className='header_name'>
+      <h1>Mazario</h1></div>
+      <div className='navigations'>
+      <Link to='/' className='a'>Home</Link>
+      <Link to='/about' className='a'>About</Link>
+      <Link to='/contact' className='a'>Contact</Link>
+      <Link to='/book' className='a'>Book</Link>
+      <Link to='/login' className='a'>Login</Link>
+      </div>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='/book' element={<Book />}/>
+      <Route path='/login' element={<Login />}/>
+    </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
